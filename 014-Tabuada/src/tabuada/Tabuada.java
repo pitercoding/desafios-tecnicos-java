32c643d318d77cd7edab2eceaf2ee6fa57f6a772
Piter Gomes <142267146+pitercoding@users.noreply.github.com>
2025-07-07 21:06:24 +0200

    Desafio 014. Tabuada de um número

--- 014-Tabuada/src/tabuada/Tabuada.java
+++ 014-Tabuada/src/tabuada/Tabuada.java
@@ -1,7 +1,27 @@
 package tabuada;
 
+import java.util.Scanner;
+
 public class Tabuada {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
+        try {
+            System.out.println("==== INICIALIZANDO PROGRAMA ====");
+            System.out.print("Digite um número para ver a tabuada: ");
+            int numero = scanner.nextInt();
+
+            System.out.println("\nTabuada de " + numero + ":");
+
+            for (int i = 1; i <= 10; i++) {
+                int resultado = numero * i;
+                System.out.println(numero + " x " + i + " = " + resultado);
+            }
+            System.out.println("=====================");
+        } catch (Exception e) {
+            System.out.println("[ERRO!]Informe um número inteiro válido!");
+        } finally {
+            scanner.close();
+        }
     }
 }
