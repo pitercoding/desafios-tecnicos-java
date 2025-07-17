c9c09c50f8ba65f84ac957b1b3cf5d6d082c5e4c
Piter Gomes <142267146+pitercoding@users.noreply.github.com>
2025-07-17 17:00:37 +0200

    Desafio 030. Contar números negativos, positivos e zeros

---
+++ 030-ContaNumeros/src/conta/numeros/ContaNumeros.java
@@ -0,0 +1,39 @@
+package conta.numeros;
+
+import java.util.Scanner;
+
+public class ContaNumeros {
+    public static void main(String[] args) {
+        /**
+         * 030. Contar números negativos, positivos e zeros
+         * O usuário digita 10 números.
+         * Conte quantos são negativos, positivos ou zero.
+         */
+        Scanner scanner = new Scanner(System.in);
+
+        int contador = 0;
+        int contadorNegativos = 0;
+        int contadorPositivos = 0;
+        int contadorZeros = 0;
+
+        do {
+            System.out.printf("Informe o %dº número: ", contador + 1);
+            int valor = scanner.nextInt();
+
+            if (valor < 0) {
+                contadorNegativos++;
+            } else if (valor > 0) {
+                contadorPositivos++;
+            } else if (valor == 0) {
+                contadorZeros++;
+            }
+            contador++;
+        } while (contador != 10);
+
+        System.out.printf("\nAo todo foram digitados: ");
+        System.out.printf("\n%d números negativos.", contadorNegativos);
+        System.out.printf("\n%d números positivos.", contadorPositivos);
+        System.out.printf("\n%d números zeros.", contadorZeros);
+        scanner.close();
+    }
+}
