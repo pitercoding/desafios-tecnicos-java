f6fc678750bc78f5f93988d07b87f20327904321
Piter Gomes <142267146+pitercoding@users.noreply.github.com>
2025-07-17 14:32:29 +0200

    Desafio 027. Soma de 10 números digitados

---
+++ 027-SomaComLaco/src/soma/SomaComLaco.java
@@ -0,0 +1,24 @@
+package soma;
+
+import java.util.Scanner;
+
+public class SomaComLaco {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int contador = 0;
+        int soma = 0;
+
+        do {
+            System.out.printf("Informe o %dº número: ", contador + 1);
+            int valor = scanner.nextInt();
+
+            soma += valor;
+            contador++;
+
+        } while (contador != 10);
+
+        System.out.println("\nA soma dos valores é " + soma);
+        scanner.close();
+    }
+}
