426fc088a074c32e8a06b051c4f94f0fd34c0100
pitercoding <piterg.bio@gmail.com>
2025-09-13 20:14:42 +0200

    reorganizando desafios em níveis

--- 000-HelloWorld/src/Main.java
+++
@@ -1,8 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        String nome = "Piter Douglas Ferreira Gomes";
-        System.out.println("Hello World!");
-        System.out.println(nome);
-
-    }
-}
