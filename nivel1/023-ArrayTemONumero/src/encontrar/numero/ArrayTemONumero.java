3288134f1101fecec3122ebede6052182632ebc4
pitercoding <piterg.bio@gmail.com>
2025-09-17 20:08:52 +0200

    exercício: 023. Verificar se um número existe dentro de um array

---
+++ nivel1/023-ArrayTemONumero/src/encontrar/numero/ArrayTemONumero.java
@@ -0,0 +1,29 @@
+package encontrar.numero;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class ArrayTemONumero {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Verifique se um número foi sorteado (Escolha entre 1 a 50): ");
+        int[] numerosaleatorios = new int[20];
+
+        for (int i = 0; i < numerosaleatorios.length; i++) {
+            numerosaleatorios[i] = (int) (Math.random() * 50 ) + 1;
+        }
+
+        int palpite = scanner.nextInt();
+        boolean existe = Arrays.stream(numerosaleatorios).anyMatch(n -> n == palpite);
+
+        if (existe) {
+            System.out.println("O número " + palpite + " foi encontrado no array!");
+        } else {
+            System.out.println("\nO número não foi encontrado!");
+        }
+
+        System.out.println("Números sorteados: " + Arrays.toString(numerosaleatorios));
+    }
+
+}
