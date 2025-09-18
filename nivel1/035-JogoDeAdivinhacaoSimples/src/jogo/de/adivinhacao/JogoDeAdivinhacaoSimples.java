c651dd4be43beb0945423490e79aae1d4a87d7c5
pitercoding <piterg.bio@gmail.com>
2025-09-18 17:21:39 +0200

    exercício: 035. Jogo de adivinhação simples (1 a 10)

---
+++ nivel1/035-JogoDeAdivinhacaoSimples/src/jogo/de/adivinhacao/JogoDeAdivinhacaoSimples.java
@@ -0,0 +1,46 @@
+package jogo.de.adivinhacao;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class JogoDeAdivinhacaoSimples {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+        int numeroSecreto = random.nextInt(10) + 1;
+
+        System.out.println("===== Jogo da Adivinhação =====");
+        System.out.println("Digite seu palpipe entre 1 e 10! Você tem 5 tentativas!");
+
+        int tentativas = 0;
+        boolean acertou = false;
+
+        while (tentativas < 5) {
+            System.out.print((tentativas + 1) + "ª tentativa: ");
+            int palpite = scanner.nextInt();
+
+            if (palpite < 1 || palpite > 10) {
+                System.out.println("[ERRO!] Número inválido. Informe um número entre 1 e 10!");
+                continue;
+            }
+
+            tentativas++;
+
+            if (palpite == numeroSecreto) {
+                System.out.printf("\nParabéns!!! Você acertou com %d tentativa(s). O número secreto é %d.\n", tentativas, numeroSecreto);
+                acertou = true;
+                break;
+            } else if (palpite < numeroSecreto) {
+                System.out.println("Errado! O número secreto é maior.");
+            } else {
+                System.out.println("Errado! O número secreto é menor.");
+            }
+        }
+
+        if (!acertou) {
+            System.out.printf("\nFim das tentativas. Você perdeu! O número secreto era %d.\n", numeroSecreto);
+        }
+
+        scanner.close();
+    }
+}
