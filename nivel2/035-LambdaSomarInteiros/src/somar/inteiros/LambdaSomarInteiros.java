df36c17495a99f58ba3492f9c41902412e1a8e8e
pitercoding <piterg.bio@gmail.com>
2025-09-22 20:29:39 +0200

    feat(exercícios):035. Use reduce com lambda para somar os valores de uma lista de inteiros

---
+++ nivel2/035-LambdaSomarInteiros/src/somar/inteiros/LambdaSomarInteiros.java
@@ -0,0 +1,12 @@
+package somar.inteiros;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class LambdaSomarInteiros {
+    public static void main(String[] args) {
+        List<Integer> listaDeInteiros = Arrays.asList(1,2,3,4,5,6,7,8,9,10);
+        System.out.println(listaDeInteiros.stream()
+                .reduce(0, (a,b) -> a + b));
+    }
+}
