cff01a80276810a661efe468b56fb18bd0cbf44e
Piter Gomes <142267146+pitercoding@users.noreply.github.com>
2025-09-22 13:27:43 +0200

    feat(exercícios): 033. Dada uma lista de inteiros, use stream() + filter com lambda para retornar apenas os pares.

---
+++ nivel2/033-LambdaSomentePares/src/somente/pares/LambdaSomentePares.java
@@ -0,0 +1,13 @@
+package somente.pares;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class LambdaSomentePares {
+    public static void main(String[] args) {
+        List<Integer> listaDeInteiros = Arrays.asList(1,2,3,4,5,6,7,8,9,10);
+        listaDeInteiros.stream()
+                .filter(n -> n % 2 == 0)
+                .forEach(System.out::println);
+    }
+}
