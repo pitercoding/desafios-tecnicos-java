70d6feed2cc0949856b4c610c42736fb1898b3c8
pitercoding <piterg.bio@gmail.com>
2025-09-22 20:56:23 +0200

    feat(exercícios): 037. Use Collections.sort ou list.sort com uma expressão lambda como comparador. Objetivo é ordenar uma lista de Strings.

---
+++ nivel2/037-LambdaOrdenarListaStrings/src/ordenar/strings/LambdaOrdenarListaStrings.java
@@ -0,0 +1,12 @@
+package ordenar.strings;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class LambdaOrdenarListaStrings {
+    public static void main(String[] args) {
+        List<String> listaDeCompras = Arrays.asList("Feijão", "Arroz", "Macarrão", "Alface", "Tomate", "Vinho", "Água", "Batata");
+        listaDeCompras.sort((a , b) -> a.compareTo(b));
+        System.out.println("Lista de Compras Ordenada: " + listaDeCompras);
+    }
+}
