e6802eed47a885f2a8a09e5043cf4eaf6807d656
pitercoding <piterg.bio@gmail.com>
2025-09-26 20:19:01 +0200

    feat(exercícios): percorrer lista de inteiros com Iterator e Stream

---
+++ nivel2/040-LambdaPercorrerListaInteiros/src/percorrerlista/PercorrerListaInteiros.java
@@ -0,0 +1,23 @@
+package percorrerlista;
+
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+
+public class PercorrerListaInteiros {
+    public static void main(String[] args) {
+        List<Integer> inteiros = Arrays.asList(1,2,3,4,5,6,7,8,9, 10);
+
+        // Parte 1: Usando Iterator
+        System.out.println("Percorrendo com Interator: ");
+        Iterator<Integer> iterator = inteiros.iterator();
+        while (iterator.hasNext()) {
+            Integer numero = iterator.next();
+            System.out.println(numero);
+        }
+
+        // Parte 2: Usando Stream
+        System.out.println("\nPercorrendo com Stream:");
+        inteiros.stream().forEach(System.out::println);
+    }
+}
