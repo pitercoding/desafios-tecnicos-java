bbcb4c3bffdb9701dd40acf7cd3e38b6c37af2a5
pitercoding <piterg.bio@gmail.com>
2025-12-26 14:46:49 +0100

    feat(ex043):  Verificar se um número é positivo, negativo ou zero

---
+++ nivel1/043-VerificarNumero/src/verificar/numero/VerificarNumero.java
@@ -0,0 +1,30 @@
+package verificar.numero;
+
+import java.util.Scanner;
+
+public class VerificarNumero {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int numero;
+
+        System.out.print("Digite um número inteiro: ");
+
+        if (sc.hasNextInt()) {
+            numero = sc.nextInt();
+
+            if (numero > 0) {
+                System.out.println("O número é positivo.");
+            } else if (numero < 0) {
+                System.out.println("O número é negativo.");
+            } else {
+                System.out.println("O número é zero.");
+            }
+
+        } else {
+            System.out.println("Entrada inválida. Digite apenas números inteiros.");
+        }
+
+        sc.close();
+    }
+}
